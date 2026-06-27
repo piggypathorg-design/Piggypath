ac449a367a0ebafa221e80ac7c2d5d3860eb88c3
PiggyPath <piggypath.org@gmail.com>
2026-06-27 12:26:56 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+// 1. Change the import at the top
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'; 
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+function App() {
+  return (
+    // 2. Change <BrowserRouter> to <Router> (or <HashRouter>)
+    <Router>
+      <Routes>
+        <Route path="/" element={<Dashboard />} /> {/* structural example */}
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
